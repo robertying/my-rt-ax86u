ae742a3eab77035bb0b09ccae7243c30e350e270
Rui Ying <yingrui205@gmail.com>
2021-07-15 20:46:02 +0800

    Support custom domains in chinadns-ng

--- opt/share/chinadns-ng/update.sh
+++ opt/share/chinadns-ng/update.sh
@@ -3,4 +3,7 @@
 curl -o /opt/share/chinadns-ng/chinalist.txt https://cdn.jsdelivr.net/gh/robertying/shadowsocks-acl@main/chinalist.txt
 curl -o /opt/share/chinadns-ng/gfwlist.txt https://cdn.jsdelivr.net/gh/robertying/shadowsocks-acl@main/gfwlist.txt
 
+cat /opt/share/chinadns-ng/customchina.txt >> /opt/share/chinadns-ng/chinalist.txt
+cat /opt/share/chinadns-ng/customgfw.txt >> /opt/share/chinadns-ng/gfwlist.txt
+
 /opt/etc/init.d/S11chinadns-ng restart
